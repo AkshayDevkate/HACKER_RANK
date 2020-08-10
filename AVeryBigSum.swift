dfd20d95e5300ef0ffe895d8962421dc96ec8637
Akshay Dattatray Devkate <42045980+AkshayDevkate@users.noreply.github.com>
2020-08-11 02:58:30 +0530

    Create AVeryBigSum.swift

---
+++ AVeryBigSum.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+// Complete the aVeryBigSum function below.
+func aVeryBigSum(ar: [Int]) -> Int {
+
+var ans:Int = ar.reduce(0,+)
+
+return ans
+}
+
+
+let result = aVeryBigSum(ar: ar)
