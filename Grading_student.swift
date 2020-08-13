6574f8c266c176cfcb9d2778a3a9b4a74e3e3bad
Akshay Dattatray Devkate <42045980+AkshayDevkate@users.noreply.github.com>
2020-08-13 15:12:22 +0530

    Update Grading_student.swift

--- Grading_student.swift
+++ Grading_student.swift
@@ -10,57 +10,97 @@ import Foundation
 
 func gradingStudents(grades: [Int]) -> [Int] {
     // Write your code here
-
-    
     var grand = grades
     var answer: [Int] = []
     //var wronganswer: [Int] = []
+    
+    
+    
+    
     func grade(){
-        
+        a
         
         //var answer: [Int]
         
         for grade in grand{
+            
+            
             var reminder = grade % 5
-            
             var multiplier = grade / 5
-            
-         
-            
         
-            if reminder == 0{
-                //print(multiplier)
+            
+            
+            //bigger condition
+            
+            if grade < 38{
                 
-                
-                //print(grade)
+                print("we are above every condtion \(grade) ")
                 
                 answer.append(grade)
+            }
+            
+            else{
+            
+            
+            // condition one
+if reminder == 0 && grade != 0 {
+                                //print(multiplier)
                 
+                                //print(grade)
+          // sub condtion one
+
                 
-                }
-            if reminder >= 3{
+                                answer.append(grade)
+            print("executed in first condiotion \(grade)")
+                
+            }
+            
+
+            
+                //condition two
+if reminder >= 3  {
+    
+   
+    if grade < 38{
+        print("sub condition 3 \(grade)")
+        
+    answer.append(grade)
+        
+            print("condition of second \(grade)")
+                answer.append(grade)
+
+            
+            }
+                    //comdtion 3
+
+                
                 var ans = (multiplier + 1) * 5
                 
-                //print("reminder for \(grade) is \(reminder) ")
+                print(" executed in condition 3  \(grade) to \(ans) ")
                 //print(ans)
                 
                 answer.append(ans)
-                
-            }
-                if reminder < 3  {
-                
-                //print(multiplier)
-                //print(grade)
-                answer.append(grade)
-            }
-            
-            
-        }
-       
-        
-        
-
+    
+               // print("executed in condition two from \(grade) to  \(ans)")
+   // }
     }
+                
+    
+                //condition 4
+if reminder < 3 && reminder != 0{
+                
+    
+    print("executed in condition 4 \(grade)")
+    
+                
+                answer.append(grade)
+    }
+            
+            
+  }
+        }
+    }
+      
 
     grade()
     return answer
@@ -71,5 +111,7 @@ func gradingStudents(grades: [Int]) -> [Int] {
 
 
 
-let result = gradingStudents(grades: [73,67,38,33])
+let result = gradingStudents(grades: [44,84,94,21,0,18,100,18,62,30,65,53,0,43,2,29,53,61,40,14])
+
+
 
